d11d73cc573a9238e96cbe00f7f3dc24e9848f4e
Louis <louis.hoareau.pro@gmail.com>
2020-02-17 10:59:33 +0100

    Release 3n

--- src/service/var.service.ts
+++ src/service/var.service.ts
@@ -422,7 +422,7 @@ export class VarService {
     {name:"Soft 1", date:"11/02/20 19:30", theme:"Jeux Olympique", src:"../../assets/files/party/party_soft_1.png", spoil:new Date('2020-02-11T05:20:30Z')<new Date()},
     {name:"Archery tag", date:"13/02/20", theme:"Shooter à l'arc", src:"../../assets/files/party/Archery_Tag.png", spoil:new Date('2020-02-12T05:20:30Z')<new Date()},
     {name:"Hard 1", date:"13/02/20", theme:"Anti'Kitsch", src:"../../assets/files/party/party_hard_1.png", spoil:new Date('2020-02-13T05:20:30Z')<new Date()},
-    {name:"Soft 2", date:"17/02/20 19:15", theme:"", src:"../../assets/files/party/party_soft_2.png", spoil:new Date('2020-02-17T05:20:30Z')<new Date()},
+    {name:"Soft 2", date:"17/02/20 19:15", theme:"Enquête", src:"../../assets/files/party/party_soft_2.png", spoil:new Date('2020-02-17T05:20:30Z')<new Date()},
     {name:"Afterwork 2", date:"19/02/20 19:00", theme:"Afterwork au Delirium", src:"../../assets/files/party/afterwork_2.png", spoil:new Date('2020-02-19T05:20:30Z')<new Date()},
     {name:"Hard 2", date:"19/02/20 23:00", theme:"FeRia Antique", src:"../../assets/files/party/party_hard_2.png", spoil:new Date('2020-02-19T05:20:30Z')<new Date()},
   
